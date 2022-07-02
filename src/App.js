2fddc6ead0f0eb5451439949bd0ab44d96401c57
juanmagal10 <juanmagalvan.24@gmail.com>
2022-07-02 14:13:59 -0300

    add description and projects grid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Header from './components/header/Header';
 import Carousel from './components/Carousel/Carousel';
+import Description from './components/Description/Description';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Carousel />
+      <Description />
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Description/Description.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Description.css'
+
+const Description = () => {
+  return (
+      <section className='description'>
+          <h3 className='description-title'>Titulo de la descripcion</h3>
+          <div className='p-grid'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cum repellat amet ipsum harum expedita assumenda laudantium, voluptate iusto excepturi inventore neque? Aliquid provident magnam, minus quae illum reiciendis!</p>
+          </div>
+      </section>
+  )
+}
+
+export default Description
