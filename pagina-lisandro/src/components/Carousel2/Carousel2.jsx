324457089dabe22af270bd48834b3da87a383228
juanmagal10 <juanmagalvan.24@gmail.com>
2022-07-12 22:15:07 -0300

    carousel text animation

--- pagina-lisandro/src/components/Carousel2/Carousel2.jsx
+++ pagina-lisandro/src/components/Carousel2/Carousel2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRef, useState } from 'react';
+
 import img1 from '../../assets/img/img1.jpg'
 import img2 from '../../assets/img/img2.jpg'
 import img3 from '../../assets/img/img3.jpg'
@@ -9,47 +9,35 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/scss";
 import "swiper/scss/pagination";
+import "swiper/css/effect-fade";
 import "swiper/scss/navigation";
 import "swiper/scss/autoplay";
 import './Carousel2.scss'
 
 
 
-import { Autoplay, Pagination, Navigation } from "swiper";
+import {EffectFade, Autoplay, Pagination, Navigation } from "swiper";
+
 
 
 
 
 const Carousel2 = () => {
-  const [animation, setAnimation] = useState(true)
-
-  const addAnimation = () => {
-      setTimeout(() => {
-    setAnimation(true);
-    setTimeout(()=>{
-      const removeClass = () => {
-        setAnimation(false)
-      }
-      removeClass()
-    }, 2500)
-
-  }, 1200)
-  }
-
 
   return (
   <>
       <Swiper
         centeredSlides={true}
-        
+        effect={"fade"}
         loop={true}
-        speed={1000}
+        speed={1300}
         autoplay={{
-          delay: 2500,
+          delay: 3000,
           disableOnInteraction: false,
         }}
-        onSlideChange={addAnimation}
-
+        
+        
+        
         
      
         pagination={{
@@ -57,24 +45,30 @@ const Carousel2 = () => {
           
         }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
+        modules={[EffectFade, Autoplay, Pagination, Navigation]}
         className="carousel-container"
       >
         
           <SwiperSlide className='slide'>
-            <img className='slide-content' src={img1} alt="img1" />
-          <p className={animation?'animation slider-text':'slider-text'}>prueba de animacion para ver como sale</p>
+          <img className='slide-content' src={img1} alt="img1" />
+          <h3 className='slider-title'>titulo del projecto</h3>
+          <p className='animation'>prueba de animacion para ver como sale 1</p>  
+          <button className='project-btn'><a href="#">Mas...</a></button>
           </SwiperSlide>
         
        
           <SwiperSlide className='slide'>
-            <img className='slide-content' src={img2} alt="img1" />
-            <p className={animation?'animation slider-text':'slider-text'}>prueba de animacion para ver como sale</p>  
+          <img className='slide-content' src={img2} alt="img2" />
+          <h3 className='slider-title'>titulo del projecto</h3>
+          <p className='animation'>prueba de animacion para ver como sale 2</p>  
+         <button className='project-btn'><a href="#">Mas...</a></button>
           </SwiperSlide>
            
           <SwiperSlide >
-            <img src={img3} alt="img3" />
-            <p className={animation ? 'animation slider-text' : 'slider-text'}>prueba de animacion para ver como sale</p>
+          <img src={img3} alt="img3" />
+          <h3 className='slider-title'>titulo del projecto</h3>
+          <p className='animation'>prueba de animacion para ver como sale 3</p>
+          <button className='project-btn'><a href="#">Mas...</a></button>
           </SwiperSlide>
         
         
